f715389af9e18bfc5d63005b3844dd5009de6ad2
Luca Biasiolo <lcbiasiolo@gmail.com>
2024-12-21 15:28:07 +0100

    Aggiunte pagine per dado e oracolo

--- dndapp/urls.py
+++ dndapp/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("dndapp/", views.dndapp, name="index"),
+    path("dndapp/", views.dndapp, name="home"),
+    path("dndapp/dice", views.dice, name="dice"),
+    path("dndapp/oracle", views.oracle, name="oracle")
 ]
--- dndapp/views.py
+++ dndapp/views.py
@@ -2,5 +2,13 @@ from django.http import HttpResponse
 from django.template import loader
 
 def dndapp(request):
-  template = loader.get_template('first.html')
+  template = loader.get_template('home.html')
+  return HttpResponse(template.render())
+
+def dice(request):
+  template = loader.get_template('dice.html')
+  return HttpResponse(template.render())
+
+def oracle(request):
+  template = loader.get_template('oracle.html')
   return HttpResponse(template.render())
