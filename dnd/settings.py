e8a0bc7724f3ddea63ec9212cdda875a851aca14
Luca Biasiolo <lcbiasiolo@gmail.com>
2024-12-24 18:31:58 +0100

    Aggiunto bootstrap alla webapp

--- dnd/settings.py
+++ dnd/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dndapp'
+    'dndapp',
+    'bootstrap5'
 ]
 
 MIDDLEWARE = [
